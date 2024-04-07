ec79cd034e54f1ab88370ba63790d961e906ca55
edimez14 <edimez14@gmail.com>
2024-04-06 23:11:16 -0500

    I fixed the problem with reflex deploy, but it is a temporary solution

--- web_version/main/PasswordGenerator/PasswordGenerator.py
+++ web_version/main/PasswordGenerator/PasswordGenerator.py
@@ -1,7 +1,6 @@
 # Web Application v0.2.1
 from rxconfig import config
 import reflex as rx
-from password_generator import main as pg
 
 # components
 from PasswordGenerator.components.footer import footer
@@ -9,9 +8,10 @@ from PasswordGenerator.components.footer import footer
 # views
 from PasswordGenerator.views.header.header import header
 from PasswordGenerator.views.content.content import content_
-
+    
 class State(rx.State):
     pass
+
 
 def index() -> rx.Component:
     return rx.vstack(
--- web_version/main/PasswordGenerator/views/content/content.py
+++ web_version/main/PasswordGenerator/views/content/content.py
@@ -1,5 +1,55 @@
 import reflex as rx
-from password_generator import main as pg
+# from password_generator.main import password as pg
+
+import random
+
+# It is known that it has two methods, where one method is responsible for using the element parameter to return a list of random numbers and the second method uses the element parameter to return a list of random characters.
+class list_elements:
+    def __init__(self, element):
+        self.element = element
+
+    def select_num(self):
+        list_num = set(range(self.element))
+        list_num = random.sample(list_num, 50)
+        return list_num
+
+    def select_char(self):
+        chars = []
+        for i in range(5):
+            char = random.choice(self.element)
+            chars.append(char)
+        return list(set(chars))
+    
+
+"""
+function that requests three list type parameters that has a for loop that repeats 50 times to generate in each iteration a random password that is added to a list and then a variable randomly selects an index of that list of passwords and returns the variable with the chosen password
+"""
+def password_generator(list_num: list, list_str: list, list_char: list):
+    passwords = []
+    for i in range(50):
+        password = f"{random.choice(list_char)}{random.choice(list_num)}{random.choice(list_str)}{random.choice(list_char)}{random.choice(list_str).upper()}{random.choice(list_str)}{random.choice(list_num)}{random.choice(list_str)}{random.choice(list_str)}{random.choice(list_char)}"
+        if password not in passwords:
+            passwords.append(password)
+        else:
+            continue
+    chosen = random.choice(passwords)
+    return chosen
+
+"""
+password function that calls the classes through variables and returns the function that generates the random passwords and uses the variables as parameters
+"""
+def password():
+    num = list_elements(random.randint(1, 4000))
+    num = num.select_num()
+
+    alphabet = list_elements(list("abcdefghijklmnopqrstuvwxyz"))
+    alphabet = alphabet.select_char()
+
+    char = list_elements(['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<',
+                      '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', "¡", "¿", "¿", "¡", "°", "€"])
+    char = char.select_char()
+
+    return password_generator(num, alphabet, char)
 
 
 class Text_password(rx.State):
@@ -7,7 +57,7 @@ class Text_password(rx.State):
 
     def change_password(self):
 
-        self.password = pg.password()
+        self.password = password()
 
 
 def content_() -> rx.Component:
