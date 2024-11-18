a651a32212a59ed5dad9c14686698404dcefb3f4
edimez14 <edimez14@gmail.com>
2024-11-18 16:21:28 -0500

    update file settings.py

--- v_web_app/backend/backend/settings.py
+++ v_web_app/backend/backend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG')
-DEBUG = 'RENDER' not in os.environ.get('DEBUG')
+DEBUG = 'RENDER' not in os.environ
 
 
 # Application definition
