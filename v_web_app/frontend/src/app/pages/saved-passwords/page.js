2a65193341f2139896747402849e70b9156695f4
edimez14 <edimez14@gmail.com>
2024-11-18 15:39:50 -0500

    update file pages/saved-passwords/page.js

--- v_web_app/frontend/src/app/pages/saved-passwords/page.js
+++ v_web_app/frontend/src/app/pages/saved-passwords/page.js
@@ -1,43 +1,25 @@
 'use client';
 import { useState, useEffect } from 'react';
 
-import {
-    Dropdown,
-    DropdownTrigger,
-    DropdownMenu,
-    DropdownItem
-} from "@nextui-org/dropdown";
-import { Button } from '@nextui-org/button';
-
 import { IsAuth } from "@/app/utils/AuthContext";
 import LoadingPage from "@/app/views/LoadingPage";
 
 import SideNav from "./views/SideNav";
 import Content from "./views/Content";
-import ViewPasswords from './components/ViewPasswords';
-import SearchBar from './components/SearchBar';
 
 import "@/app/style/glassmorphism.css";
 
 export default function SavedPasswords() {
     const [selectedPassword, setSelectedPassword] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [windowSize, setWindowSize] = useState(window.innerWidth);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const isAuthenticated = IsAuth();
 
     useEffect(() => {
         setLoading(false);
-        setWindowSize(window.innerWidth);
-    }, [windowSize]);
+    }, []);
 
     const handlePasswordSelect = (password) => {
         setSelectedPassword(password);
-    };
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
