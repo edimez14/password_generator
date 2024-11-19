40c316166d3de9be9f9845be8e28c83804f77771
edimez14 <edimez14@gmail.com>
2024-11-18 21:07:58 -0500

    update file settings.py

--- v_web_app/backend/build.sh
+++ v_web_app/backend/build.sh
@@ -3,10 +3,12 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install --upgrade pip
+
 pip install -r requirements.txt
 
 # Convert static asset files
-python3 manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python3 manage.py makemigrations
