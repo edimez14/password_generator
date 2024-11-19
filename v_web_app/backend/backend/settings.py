80c370f5fe0fa10c8dac7ab4aa259ae4302ff100
edimez14 <edimez14@gmail.com>
2024-11-18 21:40:37 -0500

    update file settings.py

--- v_web_app/backend/backend/settings.py
+++ v_web_app/backend/backend/settings.py
@@ -169,22 +169,6 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEV')
-# # ALLOWED_HOSTS = []
-
-# CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS_DEV')
-
-# CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS_DEV')
-
-# CORS_ALLOW_CREDENTIALS = True
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 if not DEBUG:
     ALLOWED_HOSTS = []
 
@@ -203,5 +187,22 @@ if not DEBUG:
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             **dj_database_url.config(conn_max_age=600, ssl_require=True)
+        }
+    }
+
+elif DEBUG:
+    ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEV')
+    # ALLOWED_HOSTS = []
+
+    CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS_DEV')
+
+    CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS_DEV')
+
+    CORS_ALLOW_CREDENTIALS = True
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
